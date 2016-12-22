96e4ace45f820b9c2c2a79f8e6a2af713dc6e590
eslam_hossam <eslamhossam23@gmail.com>
2016-12-23 00:37:25 +0100

    Saved incoming values from mobile to a local database.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,34 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main {
 
+    public static final String DATABASE_URL = "jdbc:mysql://127.0.0.1:3306/ELIM";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "root";
+
     public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Couldn't initialise database class.");
+        }
         try {
             ServerSocket serverSocket = new ServerSocket(3000);
             while (true){
+                Connection connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
                 Socket socket = serverSocket.accept();
-                Thread thread = new ServerThread(socket);
+                Thread thread = new ServerThread(socket, connection);
                 thread.start();
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Something went wrong with the server/client sockets.");
+        } catch (SQLException e) {
+            System.out.println("Couldn't establish connection with database.");
         }
 
     }
--- src/ServerThread.java
+++ src/ServerThread.java
@@ -3,16 +3,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Created by Havoc on 0018-18-12-2016.
  */
 public class ServerThread extends Thread {
     private Socket socket;
+    private Connection connection;
     private final static String SEPARATOR = "-";
 
-    public ServerThread(Socket socket) {
+    public ServerThread(Socket socket, Connection connection) {
         this.socket = socket;
+        this.connection = connection;
     }
 
     @Override
@@ -25,10 +30,20 @@ public class ServerThread extends Thread {
             String line;
             while ((line = bufferedReader.readLine())!= null){
                 if(line.contains("-")){
+                    Statement statement = connection.createStatement();
                     String[] message = line.split(SEPARATOR);
                     System.out.println("Time " + message[0]);
-                    System.out.println("Latitude " + message[1]);
-                    System.out.println("Longitude " + message[2]);
+                    long time = Long.parseLong(message[0]);
+                    float db = Float.parseFloat(message[3]);
+                    if(!message[1].equals("UNKNOWN")){
+                        float latitude = Float.parseFloat(message[1]);
+                        float longitude = Float.parseFloat(message[2]);
+                        statement.executeUpdate("INSERT INTO elim.data (Time, Longitude, Latitude, dB) VALUES (" + time + ", " + longitude + ", " + latitude + ", " + db + ")");
+                        System.out.println("Latitude " + message[1]);
+                        System.out.println("Longitude " + message[2]);
+                    }else{
+                        statement.executeUpdate("INSERT INTO elim.data (Time, Longitude, Latitude, dB) VALUES (" + time + ", 0, 0, " + db + ")");
+                    }
                     System.out.println("Decibels " + message[3]);
                 }else{
                     System.out.println(line);
@@ -36,6 +51,9 @@ public class ServerThread extends Thread {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+//            System.out.println("Something went wrong while updating the database.");
         }
     }
 }
